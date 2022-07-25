a1d44d8d7f545eb9a15cef94e6a1787d5c13cb76
Robert Slocumbe <rslocumbe93@gmail.com>
2022-07-25 12:42:51 -0400

    added HTML for page-template.js

---
+++ reset.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+if (fs.existsSync('./dist/team.html')) {
+  fs.unlinkSync('./dist/team.html');
+  console.log('/dist/ folder reset!');
+}
--- src/page-template.js
+++ src/page-template.js
@@ -1,12 +1,78 @@
-const createHTML = (team) => {
-    return `
-        <div>
-            ${team.name}
-            ${team.id}
-            ${team.email}
+const generateTeam = (team) => {
+
+    const generateManager = manager => {
+        return `
+        <div class="card">
+            <div class="card-header">
+                <h2>${manager.getName()}</h2>
+                <h3>${manager.getRole()}</h3>
+            </div>
+            <div class="card-body-container">
+                <div class="card-body">
+                    <p>ID: ${manager.getId()}</p>
+                    <p>Email: ${manager.getEmail()}</p>
+                    <p>Office: ${manager.getOffice()}</p>
+                </div>
+            </div>
         </div>
-    `
-}
+        `
+    }
+
+    const generateEngineer = engineer => {
+        return `
+        <div class="card">
+            <div class="card-header">
+                <h2>${engineer.getName()}</h2>
+                <h3>${engineer.getRole()}</h3>
+            </div>
+            <div class="card-body-container">
+                <div class="card-body">
+                    <p>ID: ${engineer.getId()}</p>
+                    <p>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                    <p>GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></p>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    const generateIntern = intern => {
+        return `
+        <div class="card">
+            <div class="card-header">
+                <h2>${intern.getName()}</h2>
+                <h3>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body-container">
+                <div class="card-body">
+                    <p>ID: ${intern.getId()}</p>
+                    <p>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+                    <p>School: ${intern.getSchool()}</p>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
+};
 
 module.exports = team => {
     return `
@@ -16,10 +82,19 @@ module.exports = team => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Company Roster</title>
+        <link rel="stylesheet" href="./style.css">
+        <title>Document</title>
     </head>
     <body>
-        
+        <header id="header-container">
+            <h1 id="header-title">My Team</h1>
+        </header>
+    
+        <main>
+            <div id="container">
+                ${generateTeam()}
+            </div>
+        </main>
     </body>
     </html>`
 }
