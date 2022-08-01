927af2dc9fd5db68ffe5007570af8cd8b4817d6f
Robert Slocumbe <rslocumbe93@gmail.com>
2022-08-01 18:49:02 -0400

    created tests for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,21 +1,61 @@
 const Employee = require("../lib/Employee");
 
-test("When I call the Employee class, it will create an instance object", () => {
+describe("When I create a new Employee it", () => {
+  it("should create a new instance object", () => {
     //AAA
     //Arrange
     //Act
     const instanceObject = new Employee();
     //Assert
-    expect(typeof(instanceObject)).toBe("object");
-});
-
-test("When I call the getName method, I should see the employee's name", () => {
+    expect(typeof instanceObject).toBe("object");
+  });
+  it("should use the arguments to populate the instance properties", () => {
     //AAA
     //Arrange
-    const name = "Jodi"
+    //Act
+    const newGuy = new Employee("Jim", 34, "jim@gmail.com");
+    //Assert
+    expect(newGuy).toEqual({ name: "Jim", id: 34, email: "jim@gmail.com" });
+  });
+});
+
+describe("When calling the method", () => {
+  it("getName, it should return the employee's name", () => {
+    //AAA
+    //Arrange
+    const name = "Jodi";
     const newObj = new Employee(name);
     //Act
     const newName = newObj.getName();
     //Assert
     expect(newName).toBe(name);
+  });
+  it("getId, it should return the employee's id", () => {
+    //AAA
+    //Arrange
+    const id = 55;
+    const newObj = new Employee("Jim", id);
+    //Act
+    const newId = newObj.getId();
+    //Assert
+    expect(newId).toBe(id);
+  });
+  it("getEmail, it should return the employee's id", () => {
+    //AAA
+    //Arrange
+    const email = "email@gmail.com";
+    const newObj = new Employee("Jim", 55, email);
+    //Act
+    const newEmail = newObj.getEmail();
+    //Assert
+    expect(newEmail).toBe(email);
+  });
+  it("getRole, it should return the string 'Employee'", () => {
+    //AAA
+    //Arrange
+    //Act
+    const newObj = new Employee();
+    //Assert
+    expect(newObj.getRole()).toBe("Employee");
+  });
 });
